20278bab98b8a23d9df6ea37547dd1c062e43569
cava <105758715+cavanna11@users.noreply.github.com>
2025-05-15 20:23:20 -0300

    cuarto

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/personaltrainer/',
+  base: '/<TU_REPO>/', // Reemplaza <TU_REPO> por el nombre de tu repo
   plugins: [react()],
 });
